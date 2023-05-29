1843d4c61d1b497327e4199a55998380486998f8
samdaphbynet <lj8@hotmail.fr>
2023-05-29 18:42:48 +0200

    fixed checked

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str.split()[5] + " programming with Python"
+str = str[39:66] + str[106:112] + str[:7]
 print(str)
