5b79b3a1d8f77c76a2446d9ba79c3f5ba581fe2e
samdaphbynet <lj8@hotmail.fr>
2023-05-30 11:24:43 +0200

    update script

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-combinition = []
-for i in range(10):
+for i in range(0, 10):
     for j in range(i+1, 10):
-        combinition.append(f"{i}{j}")
-print(", ".join(combinition))
+        if i == 8 and i == 8:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
