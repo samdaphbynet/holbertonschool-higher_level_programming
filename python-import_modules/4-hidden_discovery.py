16f3c47bab8462414750a6eeb5331228f8163c0d
samdaphbynet <lj8@hotmail.fr>
2023-05-31 15:52:29 +0200

    program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import importlib.util
+
+def print_module_names():
+    spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    names = [name for name in dir(module) if not name.startswith('__')]
+    sorted_names = sorted(names)
+    for name in sorted_names:
+        print(name)
+
+if __name__ == '__main__':
+    print_module_names()
+
