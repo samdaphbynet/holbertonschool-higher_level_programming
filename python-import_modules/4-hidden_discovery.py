d284991d4f3d5052ab1c24adcb5aae697572aca3
samdaphbynet <lj8@hotmail.fr>
2023-05-31 16:08:55 +0200

    fixed pycodestyle

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import importlib.util
 
+
 def print_module_names():
     spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
     module = importlib.util.module_from_spec(spec)
@@ -10,6 +11,7 @@ def print_module_names():
     sorted_names = sorted(names)
     for name in sorted_names:
         print(name)
+
 
 if __name__ == '__main__':
     print_module_names()
