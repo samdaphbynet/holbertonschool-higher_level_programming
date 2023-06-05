182fd52254121ba298196f8990bbcbf5134cc64e
samdaphbynet <lj8@hotmail.fr>
2023-06-05 11:36:55 +0200

    update function

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,9 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return None
     else:
-        find = my_list.index(idx)
-        find = my_list[idx]
-    return find
+        return my_list[idx]
