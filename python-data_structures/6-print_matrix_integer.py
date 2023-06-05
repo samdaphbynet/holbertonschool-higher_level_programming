547fe8bb99188cb109e887fd620a3e30bb9a3a16
samdaphbynet <lj8@hotmail.fr>
2023-06-05 20:29:19 +0200

    update function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            print("{}".format(row[i]), end="")
+        for i, num in enumerate(row):
+            print("{}".format(num), end="")
             if i != len(row) - 1:
                 print(" ", end="")
         print()
