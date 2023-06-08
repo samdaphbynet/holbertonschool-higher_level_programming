27db83b2c428ff31e7d7d35faec7e4f1c20d18ce
samdaphbynet <lj8@hotmail.fr>
2023-06-08 12:37:02 +0200

    fixed pycodestyle and added nameError to except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
         print()
-        return i + 1  
-    except:
+        return i + 1
+    except NameError:
         print()
         return i
