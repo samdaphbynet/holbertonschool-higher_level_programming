9a06335b0259be8ac24c842290f9ad89a1dad2df
samdaphbynet <lj8@hotmail.fr>
2023-06-09 10:41:44 +0200

    update function

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        counter = 0
-        for i in my_list:
-            print(i, end="")
-            counter += 1
-            if counter == x:
-                break
-        print()
-        return counter
-    except NameError:
-        return counter
+    count = 0
+    for item in range(x):
+        try:
+            print(my_list[item], end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
