c10bc0812898f3d64ffc3c4580a0a3a6bcaf8b32
samdaphbynet <lj8@hotmail.fr>
2023-06-16 23:10:35 +0200

    update function

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,18 +8,29 @@ Function: matrix_divided(matrix, div) divides all elements of a matrix.
 
 def matrix_divided(matrix, div):
     """
-    Returns: matrix with all elements divided by a number
+    divides all elements of a matrix by a number
+    :param matrix: a matrix
+    :param div: a number
+    :return: a matrix
     """
-    for row in matrix:
-        if not all(isinstance(num, (int, float)) for num in row):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
+    error = "matrix must be a matrix (list of lists) of integers/floats"
+    if not isinstance(matrix, list) or not all(
+            isinstance(row, list) for row in matrix):
+        raise TypeError(error)
+    row_size = len(matrix[0])
+    if not all(len(row) == row_size for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    result = []
+    new_matrix = []
     for row in matrix:
-        result.append([round(num / div, 2) for num in row])
-    return result
+        new_row = []
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError(error)
+            result = round(num / div, 2)
+            new_row.append(result)
+        new_matrix.append(new_row)
+    return new_matrix
