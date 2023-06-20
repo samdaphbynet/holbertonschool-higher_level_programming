614cda83f959ae5d0ebdacc389be8f09bc4ce772
samdaphbynet <lj8@hotmail.fr>
2023-06-20 12:42:23 +0200

    class BaseGeometry (based on 5-base_geometry.py).

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Filename: 6-base_geometry.py
+"""
+Module : BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Improve Geometry
+    """
+    def area(self):
+        """
+        Public instance method:
+            that raises an Exception with the message
+        """
+        raise NotImplementedError("Area() is not implemented")
