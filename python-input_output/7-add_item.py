718b4dad0ec74e2c1383312fa5d559c6239aab41
samdaphbynet <lj8@hotmail.fr>
2023-06-23 19:40:48 +0200

    import os

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,6 +3,7 @@
 script that adds all arguments to a Python list
 """
 import sys
+import os
 save_to_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
