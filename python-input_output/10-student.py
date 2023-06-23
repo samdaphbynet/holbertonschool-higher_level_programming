1c92f2266569c6fe196e416b805fc3451aafb6b7
samdaphbynet <lj8@hotmail.fr>
2023-06-23 21:12:56 +0200

    class Student that defines a student by: (based on 9-student.py)

---
+++ python-input_output/10-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+class Student that defines a student by:
+    Public instance attributes
+    Instantiation : first_name, last_name and age
+    Public method : def to_json(self, attrs=None):
+"""
+import json
+
+
+class Student:
+    """
+    A class representing a Student.
+    """
+    def __init__(self, fisrt_name, last_name, age):
+        """
+        Initialize a Student instance.
+
+        Args:
+            fisrt_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = fisrt_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list, optional): List of attribute names to retrieve.
+                If None, retrieve all attributes.
+
+        Returns:
+            dict: A dictionary representing the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
