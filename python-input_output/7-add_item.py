5da897160d95129812a50d8c65652f8d7874b2a6
samdaphbynet <lj8@hotmail.fr>
2023-06-23 19:40:19 +0200

    delete try and except from function

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
+"""
+script that adds all arguments to a Python list
+"""
 import sys
 save_to_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-try:
-    # load existing data from file if it exists
-    data = load_from_json_file(filename)
-except FileNotFoundError:
-    # create an empty list if the file doesn't exist
-    data = []
-# add command line arguments to the list
+data = load_from_json_file(filename) if os.path.exists(filename) else []
 data.extend(sys.argv[1:])
 
 # Save the updated list to the file
