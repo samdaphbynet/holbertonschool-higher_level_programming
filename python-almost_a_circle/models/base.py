724f3576feccbe7c9a9cd10c7aec775a8214a4d4
samdaphbynet <lj8@hotmail.fr>
2023-06-27 21:00:37 +0200

    update return of function to_json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ This class will be the “base” of all other classes in this project.
 The goal of it is to manage id attribute in all your future classes
 and to avoid duplicating the same code (by extension, same bugs)
 """
+import json
 
 
 class Base:
@@ -33,17 +34,17 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """
-        The function "to_json_string" returns a list of 
+        The function "to_json_string" returns a list of
         dictionaries as a JSON string, or an empty list if
         the input is None.
-        
-        Param list_dictionaries: A list of dictionaries 
+
+        Param list_dictionaries: A list of dictionaries
         that needs to be converted to a JSON string
-        Return: If the input `list_dictionaries` is `None`, 
+        Return: If the input `list_dictionaries` is `None`,
         an empty list is returned. Otherwise, the input
         `list_dictionaries` is returned as is.
         """
-        if list_dictionaries is None:
-            return []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         else:
-            return list_dictionaries
+            return json.dumps(list_dictionaries)
