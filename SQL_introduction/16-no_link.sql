13337833bc8be6ef301d48fcf557f968b9205802
samdaphbynet <lj8@hotmail.fr>
2023-07-25 23:46:45 +0200

    added new data in the table

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
 -- Lists all records of the table second_table.
+INSERT INTO second_table(id, name, score) VALUES (3, 'Aria', 18), (4, 'Aria', 12);
 SELECT score, name FROM second_table ORDER BY score DESC;
