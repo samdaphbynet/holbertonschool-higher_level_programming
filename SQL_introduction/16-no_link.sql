e0e6b131ecb39e9f9be0d7dfecbef673b2e67e50
samdaphbynet <lj8@hotmail.fr>
2023-07-25 23:50:18 +0200

    update structure of code

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
 -- Lists all records of the table second_table.
-INSERT INTO second_table(id, name, score) VALUES (3, 'Aria', 18), (4, 'Aria', 12);
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name 
+FROM second_table
+WHERE name != ""
+ORDER BY score DESC;
