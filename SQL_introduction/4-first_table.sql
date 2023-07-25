98a35c3caadd91883d19b1c73523fdeb71d4cbe4
samdaphbynet <lj8@hotmail.fr>
2023-07-25 14:03:13 +0200

    update method to create table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,2 @@
 -- Create a tabale called first_table in current database in Mysql server.
-USE 'hbtn_0c_0';
-CREATE TABLE first_table (
-    id INT, 
-    name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS `first_table` ( `id` INT, `name` VARCHAR(256));
