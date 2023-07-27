423193f9db3caca79a3e6bcc3f9126c4456977a4
samdaphbynet <lj8@hotmail.fr>
2023-07-27 23:46:49 +0200

    fixed task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,9 @@
 -- Create the databases and user.
 CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
+    IF NOT EXISTS hbtn_0d_2;
 CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IF NOT EXISTS user_0d_2@localhost
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+   ON hbtn_0d_2.*
+   TO user_0d_2@localhost;
