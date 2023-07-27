6d6a1de34e7ccbc2ead8c0b0ea503422656c9cd9
samdaphbynet <lj8@hotmail.fr>
2023-07-27 22:52:36 +0200

    change name of user 2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges of the Mysql users in localhost.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_1d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
