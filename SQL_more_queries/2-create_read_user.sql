b00051d0d9efb27bf1b001e3cb860014ab4566ef
samdaphbynet <lj8@hotmail.fr>
2023-07-27 23:41:35 +0200

    update

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,11 @@
 -- Create the databases and user.
-CREATE DATABASE 
-    IF NOT EXISTS hbtn_0d_2;
-CREATE USER 
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-    ON hbtn_0d_2.*
-    TO user_0d_2@localhost
-    IDENTIFIED BY 'user_0d_2_pwd';
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
