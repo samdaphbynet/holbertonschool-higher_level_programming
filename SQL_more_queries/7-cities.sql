b667eeee8da806e21a15e0cf9c035627e78ea4d7
samdaphbynet <lj8@hotmail.fr>
2023-07-28 23:13:09 +0200

    Create database and table cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Create the database and table in the database on Mysql server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+    id INT NOT NULL AUTO_INCREMENT, 
+    state_id INT NOT NULL,
+    name VARCHAR(256),
+    PRIMARY KEY(id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
