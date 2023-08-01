825864e7d89d9dada8b75ad37c4279ef645958c1
samdaphbynet <lj8@hotmail.fr>
2023-08-01 13:51:59 +0200

    delete space

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Script that lists all states with a name
    starting with N (upper N) from the database hbtn_0e_0_usa"""
-
-
 import MySQLdb
 import sys
 
+
 if __name__ == '__main__':
+
     # Connect to MySQL server
     db = MySQLdb.connect(host="localhost",
                          port=3306,
